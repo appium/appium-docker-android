ea5cc158842b8fe5d98c3e0cba8d7ee8035017b6
Andrejs <andrejs.cunskis@gmail.com>
2017-06-05 12:48:20 +0300

    Added functionality to connect to selenium grid (#43)
    * Added functionality to connect to selenium grid
    
    * Dynamically get device properties, test for config creation
    
    * Added bats-mock module for testing
    
    * Selenium grid functionality tests with mocks

---
+++ Appium/entry_point.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NODE_CONFIG_JSON="/root/nodeconfig.json"
+CMD="xvfb-run appium"
+
+if [ ! -z "$CONNECT_TO_GRID" ]; then
+  /root/generate_config.sh $NODE_CONFIG_JSON
+  CMD+=" --nodeconfig $NODE_CONFIG_JSON"
+fi
+
+$CMD
