5360b90aeda16d20ae8df57292bd345732a37467
Aleksandr Bochev <red.avtovo@gmail.com>
2017-10-20 12:01:31 +0200

    refactored autoconnect function Updated entry point and Dockerfile

--- Appium/entry_point.sh
+++ Appium/entry_point.sh
@@ -4,12 +4,7 @@ NODE_CONFIG_JSON="/root/nodeconfig.json"
 CMD="xvfb-run appium"
 
 if [ ! -z "$REMOTE_ADB" ]; then
-    if [ -z "$REMOTE_ADB_POLLING_SEC" ]; then
-      REMOTE_ADB_POLLING_SEC=5
-    fi
     /root/wireless_connect.sh
-    #run regular check of connected devices
-    nohup watch -n ${REMOTE_ADB_POLLING_SEC} /root/wireless_connect.sh
 fi
 
 if [ ! -z "$CONNECT_TO_GRID" ]; then
