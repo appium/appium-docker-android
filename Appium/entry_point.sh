b2873c31ccd55a34daf423e92189dda825b13cb2
Alex <red-avtovo@users.noreply.github.com>
2017-10-23 12:56:05 +0200

    wireless devices autoconnection (#67)
    * refactored wireless device connection script
    added daemon for wireless device autoconnection
    
    * updated README
    
    * updated Dockerfile
    
    * refactored autoconnect function
    Updated entry point and Dockerfile

--- Appium/entry_point.sh
+++ Appium/entry_point.sh
@@ -3,6 +3,10 @@
 NODE_CONFIG_JSON="/root/nodeconfig.json"
 CMD="xvfb-run appium"
 
+if [ ! -z "$REMOTE_ADB" ]; then
+    /root/wireless_connect.sh
+fi
+
 if [ ! -z "$CONNECT_TO_GRID" ]; then
   /root/generate_config.sh $NODE_CONFIG_JSON
   CMD+=" --nodeconfig $NODE_CONFIG_JSON"
