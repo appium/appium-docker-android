48b76822406364bf1c108f6b623227de43546fe8
butomo1989 <budi.ut.1989@gmail.com>
2018-06-20 00:25:10 +0200

    Fixed if-else logic

--- Appium/entry_point.sh
+++ Appium/entry_point.sh
@@ -4,19 +4,15 @@ NODE_CONFIG_JSON="/root/nodeconfig.json"
 APPIUM_LOG="/var/log/appium.log"
 CMD="xvfb-run appium --log $APPIUM_LOG"
 
-function generate_node_config() {
-  if [ -z "$CUSTOM_NODE_CONFIG" ]; then
-    /root/generate_config.sh $NODE_CONFIG_JSON
-  fi
-}
-
-if [ ! -z "$REMOTE_ADB" ]; then
-  /root/wireless_connect.sh
+if [ "$REMOTE_ADB" = true ]; then
+	/root/wireless_connect.sh
 fi
 
-if [ ! -z "$CONNECT_TO_GRID" ]; then
-  generate_node_config
-  CMD+=" --nodeconfig $NODE_CONFIG_JSON"
+if [ "$CONNECT_TO_GRID" = true ]; then
+    if [ "$CUSTOM_NODE_CONFIG" != true ]; then
+		/root/generate_config.sh $NODE_CONFIG_JSON
+	fi
+	CMD+=" --nodeconfig $NODE_CONFIG_JSON"
 fi
 
 pkill -x xvfb-run
