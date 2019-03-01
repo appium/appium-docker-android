1bb0058359034e4bcb86d8d4f689942b527a2a0e
budtmo <budi.ut.1989@gmail.com>
2019-03-01 11:56:33 +0100

    Formatted

--- Appium/entry_point.sh
+++ Appium/entry_point.sh
@@ -5,18 +5,18 @@ APPIUM_LOG="/var/log/appium.log"
 CMD="xvfb-run appium --log $APPIUM_LOG"
 
 if [ "$REMOTE_ADB" = true ]; then
-	/root/wireless_connect.sh
+    /root/wireless_connect.sh
 fi
 
 if [ "$CONNECT_TO_GRID" = true ]; then
     if [ "$CUSTOM_NODE_CONFIG" != true ]; then
-		/root/generate_config.sh $NODE_CONFIG_JSON
-	fi
-	CMD+=" --nodeconfig $NODE_CONFIG_JSON"
+        /root/generate_config.sh $NODE_CONFIG_JSON
+    fi
+    CMD+=" --nodeconfig $NODE_CONFIG_JSON"
 fi
 
 if [ "$RELAXED_SECURITY" = true ]; then
-	CMD+=" --relaxed-security"
+    CMD+=" --relaxed-security"
 fi
 
 pkill -x xvfb-run
