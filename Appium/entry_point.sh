9f9d103cbcd7518303be79b82630b726188c8dee
RzRnt <rezirusnadi@gmail.com>
2019-04-04 23:36:15 +0700

    Add capability to set default appium capabilities

--- Appium/entry_point.sh
+++ Appium/entry_point.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NODE_CONFIG_JSON="/root/nodeconfig.json"
+DEFAULT_CAPABILITIES_JSON="/root/defaultcapabilities.json"
 APPIUM_LOG="/var/log/appium.log"
 CMD="xvfb-run appium --log $APPIUM_LOG"
 
@@ -20,6 +21,10 @@ if [ "$CONNECT_TO_GRID" = true ]; then
         /root/generate_config.sh $NODE_CONFIG_JSON
     fi
     CMD+=" --nodeconfig $NODE_CONFIG_JSON"
+fi
+
+if [ "$DEFAULT_CAPABILITIES" = true ]; then
+    CMD+=" --default-capabilities $DEFAULT_CAPABILITIES_JSON"
 fi
 
 if [ "$RELAXED_SECURITY" = true ]; then
